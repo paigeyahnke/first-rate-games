07d2b1d1096a6f77210be8a4c14ed4c8c818c729
Bao Vue <bvue@madisoncollege.edu>
2016-11-16 12:19:42 -0600

    adding more comments and the javadoc

--- src/main/java/edu/matc/firstrategames/FirstRateGamesApplication.java
+++ src/main/java/edu/matc/firstrategames/FirstRateGamesApplication.java
@@ -5,10 +5,12 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
-//Defines the base URI for all resource URIs.
-@ApplicationPath("game")
+/**
+ * The java class declares root resource and provider classes and defines the base URI for all resource URIs
+ */
 
-//The java class declares root resource and provider classes
+
+@ApplicationPath("game")
 public class FirstRateGamesApplication extends Application {
 
     //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
